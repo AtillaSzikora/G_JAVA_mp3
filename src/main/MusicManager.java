c585e832b196198983e8ddef0e26f60ddc4acc85
Papuss <mrpapsu@gmail.com>
2016-02-16 10:32:21 +0100

    Test commit - Papus

--- src/main/MusicManager.java
+++ src/main/MusicManager.java
@@ -4,5 +4,6 @@ public class MusicManager {
 
     public static void main(String[] args) {
         System.out.println("Test commit for Slack integration.");
+        /*Zsolti komment teszt*/
     }
 }
