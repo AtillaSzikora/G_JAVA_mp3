04a055a908c200272a1f6ce67dedc6025ca699cd
BontaPeter <peter.bonta@gmail.com>
2016-02-17 14:49:06 +0100

    DirectoryCreator - 2/3 part complete - needed: case insensitive

--- src/main/DirectoryCreator.java
+++ src/main/DirectoryCreator.java
@@ -1,12 +1,65 @@
 package main;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DirectoryCreator {
-
-
-
-
-    /*bekapja az ID3-tag listáját végig megy rajta, és megcsinálja a mappákat a listában szereplő tag nevek alapján,
-    ha van ilyen mappa nem csinál semmit     */
-
+    /*
+     * bekapja az ID3-tag listat vegig megy rajta, es megcsinalja a mappakat a
+     * listaban szereplo tag nevek alapjan, ha van ilyen mappa nem csinal semmit
+     */
+    List<String> resultList;
+    
+    public DirectoryCreator(List<File> musicFiles, String id3Tag) {
+	resultList = new ArrayList<String>();
+	collectTagsFromFiles(musicFiles, id3Tag);
+    }
+    
+    public List<String> getID3TagList() {
+	if (resultList.size() == 0) {
+	    System.out.println("No tags in the list.");
+	}
+	return resultList;
+    }
+    
+    
+    public void collectTagsFromFiles(List<File> mp3Files, String tagName) {
+	List<String> tagList = new ArrayList<>();
+	for (File file : mp3Files) {
+	    ID3Tag tag = ID3Tag.parse(file);
+	    if (tagName.equals("title"))  {
+		tagList.add(tag.getTitle());
+	    }
+	    if (tagName.equals("artist"))  {
+		tagList.add(tag.getArtist());
+	    }
+	    if (tagName.equals("album"))  {
+		tagList.add(tag.getAlbum());
+	    }
+	    if (tagName.equals("year"))  {
+		tagList.add(String.valueOf(tag.getYear()));
+	    }
+	    if (tagName.equals("genre")) {
+		tagList.add(GenreMapper.matchGenre(tag.getGenre()));
+	    }
+	}
+	for (String tag : tagList) {
+	    resultList.add(tag);
+	}
+    }
+    
+    public static void createFolders(List<String> tagList) {
+	for (String tagValue : tagList) {
+	    if (tagValue != null) {
+		File currentDir = new File("music\\" + tagValue);
+		if (!currentDir.exists()) {
+		    currentDir.mkdir();
+		    
+		}
+	    }
+	}
+    }
+ 
 }
