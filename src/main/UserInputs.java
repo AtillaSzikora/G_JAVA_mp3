33ec53251577951cdbc8194079213fd5e02eb648
Papuss <mrpapsu@gmail.com>
2016-02-17 12:07:07 +0100

    findDir method added, Multiple directory with the same names not solved need some work on it

--- src/main/UserInputs.java
+++ src/main/UserInputs.java
@@ -1,6 +1,38 @@
 package main;
 
+import java.io.File;
+
 public class UserInputs {
+    private String findDir(File root, String name){
+        if(root.getName().equals(name)){
+            return root.getAbsolutePath();
+        }
+
+        File[] files = root.listFiles();
+        String allRes = "";
+        if (files != null){
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    String result = findDir(file, name);
+                    if (result == null){
+                        continue;
+                    }
+                    else {
+                        return result;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
 
 
+    public static void main(String[] args) {
+        UserInputs test = new UserInputs();
+        File f = new File("C:\\CodeCool");
+
+        String teszt = test.findDir(f,"RADS");
+        System.out.println(teszt);
+    }
 }
