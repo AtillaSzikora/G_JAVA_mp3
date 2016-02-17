26463f998f3c60ff2c76acb78343f029dc46d820
atillaahun <szikora.atilla@gmail.com>
2016-02-17 14:10:16 +0100

    CategoryChooser done, read comment of class how to use it

--- src/main/CategoryChooser.java
+++ src/main/CategoryChooser.java
@@ -1,11 +1,32 @@
 package main;
 
+import java.util.Scanner;
+
+// Call this class in the MusicManager's main() method with CategoryChooser.returnCategoryName();
+// It will ask for an input and the method will return with the name (String) of the selected category.
+
 public class CategoryChooser {
 
+    static String choosenNumber;
+    static String choosenCategory;
 
-    /* Listázza a választható kategóriákat majd inputba megkapja a kivalasztott kategoria számát
-        osszehhasonlítja az inputot a valaszthatóval és lekezeli azt
+    static String printCategories() {
+        return "\n1. Title\n" + "2. Artist\n" + "3. Album\n" + "4. Year\n" + "5. Genre\n" +
+                "Please enter a number for the category you'd like to sort accordingly: "; }
 
-     */
+    static void getCategoryNumber() {
+        Scanner input = new Scanner(System.in);
+        System.out.print(printCategories());
+        choosenNumber = input.nextLine(); }
 
+    static String returnCategoryName() {
+        getCategoryNumber();
+        switch (choosenNumber) {
+            case "1": choosenCategory = "title"; break;
+            case "2": choosenCategory = "artist"; break;
+            case "3": choosenCategory = "album"; break;
+            case "4": choosenCategory = "year"; break;
+            case "5": choosenCategory = "genre"; break;
+            default: System.out.println("\nYou can only choose a number from 1 to 5!"); break; }
+        return choosenCategory; }
 }
--- src/main/MusicManager.java
+++ src/main/MusicManager.java
@@ -3,11 +3,8 @@ package main;
 public class MusicManager {
 
     public static void main(String[] args) {
-        System.out.println("Test commit for Slack integration.");
 
-
-
+        CategoryChooser.returnCategoryName();
 
     }
-
 }
